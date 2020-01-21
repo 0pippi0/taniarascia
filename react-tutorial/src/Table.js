ad0912f005ae6f03ff5d2cfc5fac23146d4dbae1
0pippi0 <j.romany91@gmail.com>
2020-01-21 15:00:11 +0900

    State

--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+
+
 const TableHeader = () => {
     return (
         <thead>
@@ -17,6 +19,9 @@ const TableBody = props => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
@@ -24,17 +29,18 @@ const TableBody = props => {
     return <tbody>{rows}</tbody>
 }
 
-class Table extends Component {
-    render() {
-        const { characterData } = this.props
+const Table = (props) =>{
+    const { characterData, removeCharacter } = props;
 
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={characterData} />
-            </table>
-        )
-    }
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData = {characterData}
+            removeCharacter = {removeCharacter} />
+        </table>
+    );
 }
+
+
 
 export default Table
