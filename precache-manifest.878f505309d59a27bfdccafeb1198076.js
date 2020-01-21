d85a3e3306919b739c6020d7d3d7be52552a65c5
0pippi0 <j.romany91@gmail.com>
2020-01-21 22:57:37 +0900

    Updates

---
+++ precache-manifest.878f505309d59a27bfdccafeb1198076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8867f717460cbf684fb4a8aab642a151",
+    "url": "/taniarascia/index.html"
+  },
+  {
+    "revision": "26370f2ef270bb23e08c",
+    "url": "/taniarascia/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "28f88d6e7bf2a5d74419",
+    "url": "/taniarascia/static/js/2.d35a2c42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/taniarascia/static/js/2.d35a2c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26370f2ef270bb23e08c",
+    "url": "/taniarascia/static/js/main.46723174.chunk.js"
+  },
+  {
+    "revision": "4ec89bc89988787b979a",
+    "url": "/taniarascia/static/js/runtime-main.fec48550.js"
+  }
+]);
